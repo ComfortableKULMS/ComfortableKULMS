2c3395bb5b4879a61502cb58601a60331cbaf5a2
wasabi-eater <yusuuke.akinari@gmail.com>
2026-04-26 16:43:54 +0900

    すべてのfetchに失敗した場合、時刻を保存しないようにする

--- src/features/entity/assignment/getAssignment.ts
+++ src/features/entity/assignment/getAssignment.ts
@@ -16,7 +16,9 @@ export const getSakaiAssignments = async (hostname: string, courses: Array<Cours
     for (const assignment of result) {
         if (assignment.status === "fulfilled") assignments.push(assignment.value);
     }
-    await toStorage(hostname, AssignmentFetchTimeStorage, new Date().getTime() / 1000);
+    if (assignments.length > 0) {
+        await toStorage(hostname, AssignmentFetchTimeStorage, new Date().getTime() / 1000);
+    }
     return assignments;
 };
 
